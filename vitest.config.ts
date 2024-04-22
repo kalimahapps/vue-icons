43481b2f6e035dacfb55b9a11a0128ee72f5b2ec
Abdul Al-Hasany <kahhar87@gmail.com>
2024-04-23 09:47:01 +1000

    fix: update test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
 import { configDefaults, defineConfig } from 'vitest/config';
 
 const updatedConfig = defineConfig({
+	root: './test',
 	test: {
 		exclude: [
 			...configDefaults.exclude,
 			'packages',
 		],
-		dir: 'test',
 	},
 });
 
